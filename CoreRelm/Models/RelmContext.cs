4551cc0720e778991114dff6a9f3d5825dfba270
jdaugherty-bdl <jdaugherty@bostondatalabs.com>
2025-01-27 17:05:33 -0500

    add contexts

---
+++ CoreRelm/Models/RelmContext.cs
@@ -0,0 +1,13 @@
+﻿using CoreRelm.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoreRelm.Models
+{
+    public class RelmContext : IRelmContext
+    {
+    }
+}
