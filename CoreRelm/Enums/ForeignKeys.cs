d18fdb8f861b5836fbd9ef0ecb8dd0f9c3356b5c
jdaugherty-bdl <19158666+jdaugherty-bdl@users.noreply.github.com>
2026-02-04 11:24:46 -0500

    add foreign key referential actions for when on delete is triggered

---
+++ CoreRelm/Enums/ForeignKeys.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoreRelm.Enums
+{
+    public class ForeignKeys
+    {
+        public enum ReferentialAction
+        {
+            NoAction,
+            Cascade,
+            SetNull,
+            SetDefault,
+            Restrict
+        }
+    }
+}
