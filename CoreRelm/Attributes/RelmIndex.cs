b0420846ff63339b7ffc84496df1486ab9d1c8dd
jdaugherty-bdl <19158666+jdaugherty-bdl@users.noreply.github.com>
2026-02-04 11:19:52 -0500

    add relm index and relm unique class attributes (not wired up yet)

---
+++ CoreRelm/Attributes/RelmIndex.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoreRelm.Attributes
+{
+    /// <summary>
+    /// Specifies that the decorated class or struct should have an index created on the specified properties when used
+    /// with a Relm database.
+    /// </summary>
+    /// <remarks>Apply this attribute to a class or struct to indicate which properties should be indexed for
+    /// improved query performance. Indexing can speed up lookups on the specified properties but may increase storage
+    /// requirements and affect write performance. This attribute is intended for use with the Relm object
+    /// database.</remarks>
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct, AllowMultiple = true)]
+    public sealed class RelmIndex : Attribute
+    {
+        /// <summary>
+        /// Gets or sets the names of the properties that are indexed for search or filtering operations.
+        /// </summary>
+        public string[]? IndexedProperties { get; set; }
+
+        /// <summary>
+        /// Initializes a new instance of the RelmIndex class with the specified properties to be indexed.
+        /// </summary>
+        /// <param name="indexedProperties">An array of property names to be indexed. Must contain at least one element.</param>
+        /// <exception cref="ArgumentException">Thrown if indexedProperties is null or empty.</exception>
+        public RelmIndex(string[] indexedProperties)
+        {
+            if ((indexedProperties?.Length ?? 0) == 0)
+                throw new ArgumentException("RelmIndex indexed properties must be provided.", nameof(indexedProperties));
+
+            IndexedProperties = indexedProperties;
+        }
+    }
+}
---
+++ CoreRelm/Attributes/RelmUnique.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoreRelm.Attributes
+{
+    /// <summary>
+    /// Specifies that the decorated class or struct must have compound unique values for the specified properties.
+    /// </summary>
+    /// <remarks>Apply this attribute to a class or struct to indicate that a combination of the specified
+    /// properties should be unique across all instances. This is typically used to enforce uniqueness constraints in
+    /// data models or persistence layers.</remarks>
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct, AllowMultiple = true)]
+    public sealed class RelmUnique : Attribute
+    {
+        /// <summary>
+        /// Gets or sets the names of the properties that are subject to constraints.
+        /// </summary>
+        public string[]? ConstraintProperties { get; set; }
+
+        /// <summary>
+        /// Initializes a new instance of the RelmUnique class with the specified constraint properties.
+        /// </summary>
+        /// <param name="constraintProperties">An array of property names that define the uniqueness constraint. Cannot be null or empty.</param>
+        /// <exception cref="ArgumentException">Thrown if constraintProperties is null or contains no elements.</exception>
+        public RelmUnique(string[] constraintProperties)
+        {
+            if ((constraintProperties?.Length ?? 0) == 0)
+                throw new ArgumentException("RelmUnique constraint properties must be provided.", nameof(constraintProperties));
+
+            ConstraintProperties = constraintProperties;
+        }
+    }
+}
